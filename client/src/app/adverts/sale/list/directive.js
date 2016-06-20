4ee7b142df5808e9dbebd2f9f3c53d87d3aa2f57
Brian <briansoufir4@hotmail.com>
2016-06-21 01:51:35 +0200

    Finition module list et suppression annonce vente

--- client/src/app/adverts/sale/list/directive.js
+++ client/src/app/adverts/sale/list/directive.js
@@ -10,17 +10,32 @@
             controllerAs: 'vm',
             bindToController: true,
             /* jshint unused:false*/
-            controller: function($log, SaleAdvertsService) {
+            controller: function($log,$state, SaleAdvertsService,DeleteSaleAdvertService) {
                 var vm=this;
                 SaleAdvertsService.getSaleAdverts()
                     .then(function(saleAdverts) {
-                        $log.debug('SaleAdverts in directives :',saleAdverts.data);
+                        $log.debug('list SaleAdverts in directives :',saleAdverts.data);
                         vm.saleAdverts = saleAdverts.data;
 
                     }, function(error){
                         $log.error('Error SaleAdverts', error);
                     });
                 
+                vm.deleteSaleAdvert = function(id){
+                    $log.debug('delete : ');
+                    $log.debug(id);
+
+                    DeleteSaleAdvertService.deleteSaleAdvert(id)
+                    .then(function(SaleAdvert) {
+                        $log.debug('directives delete SaleAdvert :',SaleAdvert.data);
+                        //vm.agencies = agencies.data;
+                        $state.reload('root.sale-adverts');
+                        //alert('L\'annonce ('+SaleAdvert.data.name+') a été supprimé');
+
+                    }, function(error){
+                        $log.error('Error SaleAdvert', error);
+                    });
+                };
             },
             link: function(scope, elm, attrs){
             }
