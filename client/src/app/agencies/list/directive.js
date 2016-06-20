85e980c232e7de008b6db542e1eba6ecfb4a8381
Brian <briansoufir4@hotmail.com>
2016-06-21 01:51:52 +0200

    Finition module list et suppression agence

--- client/src/app/agencies/list/directive.js
+++ client/src/app/agencies/list/directive.js
@@ -10,7 +10,7 @@
             controllerAs: 'vm',
             bindToController: true,
             /* jshint unused:false*/
-            controller: function($log, ListAgenciesService) {
+            controller: function($log,$state, ListAgenciesService,DeleteAgenciesService) {
                 var vm=this;
                 ListAgenciesService.getAgencies()
                     .then(function(agencies) {
@@ -20,6 +20,23 @@
                     }, function(error){
                         $log.error('Error agencies', error);
                     });
+
+
+                vm.deleteAgencie = function(id){
+                    $log.debug('delete : ');
+                    $log.debug(id);
+
+                    DeleteAgenciesService.deleteAgencie(id)
+                    .then(function(agencie) {
+                        $log.debug('agencies in directives :',agencie.data);
+                        //vm.agencies = agencies.data;
+                        $state.reload('root.agencies');
+                        //alert('L\'agence ('+agencie.data.name+') a été supprimé');
+
+                    }, function(error){
+                        $log.error('Error agencies', error);
+                    });
+                };
                 
             },
             link: function(scope, elm, attrs){
--- client/src/app/agencies/list/factory.js
+++ client/src/app/agencies/list/factory.js
@@ -25,8 +25,29 @@
         return service;
     }
 
+    function DeleteAgenciesService($http,$log,API){
+        var service = {};
+        service.agencies = [];
+
+        service.deleteAgencie = function (id) {
+            $log.debug('delete agencie : ',id);
+            return $http.delete(API.URL+'agencies/'+id)
+            .success(function(data) {
+                $log.debug('delete :  ', data);
+                //service.agencies = data
+            })
+            .error(function(error) {
+                $log.error('Error', error);
+            })
+
+        };
+        
+        return service;
+    }
+    
 angular.module('app.services.agencies.list', [])
     .factory('ListAgenciesService', ListAgenciesService)
-    .factory('AgenciesService', ListAgenciesService);
+    .factory('AgenciesService', ListAgenciesService)
+    .factory('DeleteAgenciesService', DeleteAgenciesService);
 })()
 
